5aa12fb265321dd9da55a1d5326cf402376f8818
Gauntlet <ksowdi@gmail.com>
2011-04-28 15:31:59 +0100

    second commit: added time limit on inserting into queue

--- sim_class.h
+++ sim_class.h
@@ -1,6 +1,7 @@
 #ifndef sim_class
 #define sim_class
 
+#include <queue>
 #include <iostream>
 #include <ctime>
 #include "sphere_class.h"
@@ -13,6 +14,11 @@ namespace kspace {
 	private:
 		double current_time;
 		double end_time;
+		double previous_time;
+		double step_time;
+		double step_size;
+		int stime;
+		int ftime;
 
 		double D0; //the initial diameter of the spheres
 		double gamma; //the growth rate of the radii of the spheres
@@ -28,7 +34,7 @@ namespace kspace {
 		vector<sphere> spheres;
 		priority_queue<cEvent*, vector<cEvent*>, compare_events> event_q;
 	public:
-		sim(double end_time, double initial_diameter, double Growth_Rate, int width, int height, int depth, double cell_width, double cell_height, double cell_depth); //initialises the sim
+		sim(double end_time, double time_step_size, double initial_diameter, double Growth_Rate, int width, int height, int depth, double cell_width, double cell_height, double cell_depth); //initialises the sim
 		~sim();
 
 		void run();
@@ -38,7 +44,7 @@ namespace kspace {
 		//get methods
 		double get_end_time() const;
 		double growth_rate() const;
-		double diameter(double t) const;
+		double init_diameter() const;
 
 		//calculates all potential future events for the specified sphere and adds them to the queue
 		void calculate_events(const int i, const int j, const int k); 
@@ -59,15 +65,21 @@ namespace kspace {
 
 		//pause
 		void pause();
+
+		//print arrays
+		void print(double *arr, int size);
 	};
 }
 
 
 using namespace kspace;
 
-sim::sim(double end_time, double initial_diameter, double Growth_Rate, int width, int height, int depth, double cell_width, double cell_height, double cell_depth) {
+sim::sim(double end_time, double time_step_size, double initial_diameter, double Growth_Rate, int width, int height, int depth, double cell_width, double cell_height, double cell_depth) {
 	current_time = 0;
+	previous_time = 0;
 	this->end_time = end_time;
+	step_time = 0;
+	step_size = time_step_size;
 
 	accepted_counter = 0;
 	discarded_counter = 0;
@@ -77,6 +89,9 @@ sim::sim(double end_time, double initial_diameter, double Growth_Rate, int width
 	//set the growth rate and the initial diameter
 	D0 = initial_diameter;
 	gamma = Growth_Rate;
+
+	stime = 0;
+	ftime = 0;
 
 	//empty the event queue if it has some events stored already, probably unecessary 
 	if(event_q.size()) {
@@ -122,6 +137,7 @@ sim::~sim() {
 
 void sim::run() {
 	int stime = clock();
+	int st(0), ft(0);
 
 	cEvent *ev;
 
@@ -133,7 +149,15 @@ void sim::run() {
 			advance(ev);
 			delete ev;
 		} else {
-			cout << "Calculating Events" << endl;
+
+			step_time += step_size;
+
+			while(event_q.size()) {
+				ev = event_q.top();
+				event_q.pop();
+				delete ev;
+			}
+
 			for(int a = 0; a < dim[0]; a++) {
 				for(int b = 0; b < dim[1]; b++) {
 					for(int c = 0; c < dim[2]; c++) {
@@ -141,14 +165,94 @@ void sim::run() {
 					}
 				}
 			}
-			cout << "Calculations Finished" << endl;
+
+			if(st == 0) {
+				st = clock();
+			} else {
+				ft = clock();
+				double realt = (double) (ft - st);
+				double simt = (current_time - previous_time)*1000;
+				cout << "sim: "<< simt << " | real: " << realt << " | sim/real = " << simt / realt << endl;
+				previous_time = current_time;
+				st = clock();
+			}
+
+			cout << "T: " << current_time << " | QS: " << event_q.size() << " | #E: " << accepted_counter << " | #D: " << discarded_counter << " | #SEV: " << sev_counter << " | #BEV: " << bev_counter << endl;
+			make_concurrent();
+			save("save.txt");
 		}
 
 	} while(current_time < end_time);
 
 	int ftime = clock();
 
+	make_concurrent();
+	cout << "Packing Fraction = " << 4*4*atan(1.)*pow((init_diameter() + growth_rate()*current_time)/2, 3) / 3 << endl;
+
 	cout << "Run Time: " << ftime - stime << " | " << (double) (ftime - stime) / 1000 << endl;
+}
+
+//advances the simulation
+void sim::advance(cEvent *ev) {
+	int *id1, *id2;
+	bool valid_event = true;
+	int sphere_count = ev->get_sphere_count();
+	double dt;
+
+	current_time = ev->get_time(); //advance the current time to the event time
+
+	for(int a = 0; a < sphere_count; a++) {
+		id1 = ev->get_id(a);
+		sphere &s = get_sphere(id1[0], id1[1], id1[2]);
+
+		if(valid_event && (s.get_event_count() - ev->get_event_count(a) != 0)) {
+			valid_event = false;
+		}
+		
+		//advance the sphere to the currentd time
+		dt = current_time - s.get_time();
+		s.set_pos(s.get_pos(0) + s.get_vel(0)*dt, 
+				  s.get_pos(1) + s.get_vel(1)*dt,
+				  s.get_pos(2) + s.get_vel(2)*dt);
+		s.set_time(current_time);
+	}
+
+	if(valid_event) {
+		accepted_counter++;
+		id1 = ev->get_id(0);
+
+		if(sphere_count == 1) {
+			calculate_boundary_velocity(id1[0], id1[1], id1[2]); //calculate the new velocity of the sphere
+			get_sphere(id1[0], id1[1], id1[2]).inc_events();
+			bev_counter++;
+		} else if(sphere_count > 1) {
+			for(int a = 1; a < sphere_count; a++) {
+				id2 = ev->get_id(a);
+				calculate_sphere_velocity(id1[0], id1[1], id1[2], id2[0], id2[1], id2[2]); //calculate the new velocity of bothe spheres
+				get_sphere(id2[0], id2[1], id2[2]).inc_events();
+			}
+			get_sphere(id1[0], id1[1], id1[2]).inc_events();
+			sev_counter++;
+		}
+
+		//calculate the next event for all spheres involved in this event
+		for(int a = 0; a < sphere_count; a++) {
+			id1 = ev->get_id(a);
+			id2 = get_sphere(id1[0], id1[1], id1[2]).get_id(); //get the actual id
+			calculate_events(id2[0], id2[1], id2[2]); //calculate the new events for the sphere
+		}
+	} else {
+		discarded_counter++;
+
+		//calculate the next event for all spheres involved in this event
+		for(int a = 0; a < sphere_count; a++) {
+			id1 = ev->get_id(a);
+			if(get_sphere(id1[0], id1[1], id1[2]).get_event_count() - ev->get_event_count(a) == 0) {
+				id2 = get_sphere(id1[0], id1[1], id1[2]).get_id(); //get the actual id
+				calculate_events(id2[0], id2[1], id2[2]); //calculate the new events for the sphere
+			}
+		}
+	}
 }
 
 sphere& sim::get_sphere(const int i, const int j, const int k) {
@@ -174,18 +278,20 @@ double sim::growth_rate() const {
 	return gamma;
 }
 
-double sim::diameter(double t) const {
-	return (D0 + growth_rate()*t);
+double sim::init_diameter() const {
+	return D0;
 }
 
 void sim::calculate_events(const int i, const int j, const int k) {
 	double sev;
-	bool create_new_event;
 	vector<cEvent*> events;
-	cEvent *ev;
 	sphere &s = get_sphere(i, j, k);
 	
 	double bev = calculate_boundary_event(i, j, k); //calculate the time at which collision with boundary will occur
+
+	cEvent *ev = new cEvent();
+	ev->set_time(-1);
+	ev->add_sphere(i, j, k, s.get_event_count());
 
 	//if this is greater than or equal to zero calculate the times at whill collisions with other spheres will occur
 	if(bev >= 0) {
@@ -195,30 +301,24 @@ void sim::calculate_events(const int i, const int j, const int k) {
 				for(int c = s.get_lower_clim(2) - 1; c <= s.get_upper_clim(2); c++) {
 					if(a != i || b != j || c != k) {
 						sev = calculate_sphere_event(i, j, k, a, b ,c); //calculate the time at which a collision with sphere[a, b, c] will occur
-
-						if(sev >= 0 && sev <= bev) {
-							//if the time is greater than or equal to zero and less than or equal to the time at which sphere[i, j, k] 
-							//collides with its cell boundary then check to see if it occurs at the same time as another sphere-sphere collision
-							create_new_event = true;
-							for(unsigned int q = 0; q < events.size(); q++) {
-								if(round(events.at(q)->get_time() - sev) == 0) {
-									//add sphere[a, b, c] to the event
-									events.at(q)->add_sphere(a, b, c, get_sphere(a, b, c).get_event_count());
-									create_new_event = false;
-									break;
-								}
-							}
-
-							if(create_new_event) {
-								//if there are no other events which occur at the same time then create a new one and add it both to the queue
-								//and the vector 'events'
-								ev = new cEvent();
+						
+						if(sev >= 0) {
+							//compare the new event time with the previous minimum and pick the minimum one
+							if(ev->get_time() < 0) {
 								ev->set_time(sev);
-								ev->add_sphere(i, j, k, s.get_event_count());
 								ev->add_sphere(a, b, c, get_sphere(a, b, c).get_event_count());
-								events.push_back(ev);
-								event_q.push(ev);
-								ev = NULL;
+							} else {
+								if(round(ev->get_time() - sev) == 0) {
+									//if the newly calculated event occurs at the same time as a previous event add the new sphere
+									//to the event
+									ev->add_sphere(a, b, c, get_sphere(a, b, c).get_event_count());
+								} else if (ev->get_time() > sev) {
+									//if the newly calculated event occurs sooner remove all previous spheres (except for the first one)
+									//and add the new one
+									ev->reset();
+									ev->set_time(sev);
+									ev->add_sphere(a, b, c, get_sphere(a, b, c).get_event_count());
+								}
 							}
 						}
 					}
@@ -226,11 +326,21 @@ void sim::calculate_events(const int i, const int j, const int k) {
 			}
 		}
 
-		//add the boundary collision to the queue
-		ev = new cEvent();
-		ev->set_time(bev);
-		ev->add_sphere(i, j, k, s.get_event_count());
-		event_q.push(ev);
+		if((bev < ev->get_time() || ev->get_time() < 0) && bev <= step_time) {
+			delete ev;
+			if(bev <= step_time) {
+				//add the boundary collision to the queue
+				ev = new cEvent();
+				ev->set_time(bev);
+				ev->add_sphere(i, j, k, s.get_event_count());
+				event_q.push(ev);
+			}
+		} else if(ev->get_time() >= 0 && ev->get_time() <= step_time) {
+			//add the sphere-sphere collision to the queue
+			event_q.push(ev);
+		} else {
+			delete ev;
+		}
 	}
 }
 
@@ -299,12 +409,13 @@ double sim::calculate_sphere_event(const int i, const int j, const int k, const 
 	else if (n >= dim[2]) {advPos2[2] = advPos2[2] + dim[2]*cell_dim[2];}
 
 	//calculate the vector difference between the advanced positions and the velocities of the spheres
-	double p[3], v[3], np[3], P(0), V;
+	double p[3], v[3], np[3], V;
 	for(int a = 0; a < 3; a++) {
 		p[a] = advPos2[a] - advPos1[a];
 		v[a] = s2.get_vel(a) - s1.get_vel(a);
-		P += p[a]*p[a];
 	}
+
+	double P = p[0]*p[0] + p[1]*p[1] + p[2]*p[2];
 
 	double N = sqrt(P);
 
@@ -316,15 +427,15 @@ double sim::calculate_sphere_event(const int i, const int j, const int k, const 
 	//calculate the magnitude velocity along np
 	V = v[0]*np[0] + v[1]*np[1] + v[2]*np[2];
 
-	double D_max_t = diameter(max_t); //the diameter of the spheres at t(maximum)
+	double D_max_t = init_diameter() + growth_rate()*max_t; //the diameter of the spheres at t(maximum)
 	double dN = round(sqrt(P) - D_max_t);
 
 	
 	if(dN < 0) {
 		cout << "INTERSECTION/TOUCH: " << dN << " | V: " << V << endl;
-		//s1.print();
-		//s2.print();
-		//pause();
+		s1.print();
+		s2.print();
+		pause();
 	}
 	
 
@@ -336,8 +447,8 @@ double sim::calculate_sphere_event(const int i, const int j, const int k, const 
 		double A, B, C, discriminant, E, root1(-1), root2(-1);
 
 		A = v[0]*v[0] + v[1]*v[1] + v[2]*v[2] - growth_rate()*growth_rate(); //coefficient of the t^2 term
-		B = p[0]*v[0] + p[1]*v[1] + p[2]*v[2] -  D_max_t*growth_rate(); //coefficient of the t^2 term
-		C = P - D_max_t*D_max_t; //coefficient of the t^0 term;
+		B = p[0]*v[0] + p[1]*v[1] + p[2]*v[2] - D_max_t*growth_rate(); //coefficient of the t^2 term
+		C = p[0]*p[0] + p[1]*p[1] + p[2]*p[2] - D_max_t*D_max_t; //coefficient of the t^0 term;
 		discriminant = B*B - A*C;
 
 		//calculate the roots of the quadratic equation
@@ -370,66 +481,6 @@ double sim::calculate_sphere_event(const int i, const int j, const int k, const 
 	}
 }
 
-//advances the simulation
-void sim::advance(cEvent *ev) {
-	int *id1, *id2;
-	bool valid_event = true;
-	int sphere_count = ev->get_sphere_count();
-	double dt;
-
-	current_time = ev->get_time(); //advance the current time to the event time
-
-	for(int a = 0; a < sphere_count; a++) {
-		id1 = ev->get_id(a);
-		sphere &s = get_sphere(id1[0], id1[1], id1[2]);
-
-		if(valid_event && (s.get_event_count() - ev->get_event_count(a) != 0)) {
-			valid_event = false;
-		}
-		
-		//advance the sphere to the current time
-		dt = current_time - s.get_time();
-		s.set_pos(s.get_pos(0) + s.get_vel(0)*dt, 
-				  s.get_pos(1) + s.get_vel(1)*dt,
-				  s.get_pos(2) + s.get_vel(2)*dt);
-		s.set_time(current_time);
-	}
-
-	if(valid_event) {
-		if(accepted_counter%5000 == 0) {
-			cout << "T: " << current_time << " | QS: " << event_q.size() << " | #E: " << accepted_counter << " | #D: " << discarded_counter << " | #SEV: " << sev_counter << " | #BEV: " << bev_counter << endl;
-			make_concurrent();
-			save("save.txt");
-		}
-
-		accepted_counter++;
-		id1 = ev->get_id(0);
-
-		if(sphere_count == 1) {
-			calculate_boundary_velocity(id1[0], id1[1], id1[2]); //calculate the new velocity of the sphere
-			bev_counter++;
-		} else if(sphere_count > 1) {
-			for(int a = 1; a < sphere_count; a++) {
-				id2 = ev->get_id(a);
-				calculate_sphere_velocity(id1[0], id1[1], id1[2], id2[0], id2[1], id2[2]); //calculate the new velocity of bothe spheres
-			}
-			get_sphere(id1[0], id1[1], id1[2]).inc_events();
-			sev_counter++;
-		}
-
-		id1 = ev->get_id(0);
-		calculate_events(id1[0], id1[1], id1[2]);
-
-		for(int a = 1; a < sphere_count; a++) {
-			id1 = ev->get_id(a); //get the id of the sphere relative to primary sphere
-			id2 = get_sphere(id1[0], id1[1], id1[2]).get_id(); //get the actual id
-			calculate_events(id2[0], id2[1], id2[2]); //calculate the new events for the sphere
-		}
-	} else {
-		discarded_counter++;
-	}
-}
-
 void sim::calculate_boundary_velocity(const int i, const int j, const int k) {
 	sphere &s = get_sphere(i, j, k);
 
@@ -446,8 +497,6 @@ void sim::calculate_boundary_velocity(const int i, const int j, const int k) {
 			}
 		}
 	}
-
-	s.inc_events(); //increment the sphere's number of events
 }
 
 void sim::calculate_sphere_velocity(const int i, const int j, const int k, const int l, const int m, const int n) {
@@ -503,8 +552,6 @@ void sim::calculate_sphere_velocity(const int i, const int j, const int k, const
 		}
 	}
 	//if the they are moving away from each other at a speed faster than the growth rate along np then do nothing to the velocities
-
-	s2.inc_events();
 }
 
 void sim::make_concurrent() {
@@ -546,7 +593,6 @@ void sim::save(char fname[128]) {
 	ofstream file(fname);
 	int id[3];
 	if(file.is_open()) {
-		cout << "Saving Data" << endl;
 		for(int a = 0; a < dim[0]; a++) {
 			id[0] = a;
 			for(int b = 0; b < dim[1]; b++) {
@@ -573,15 +619,28 @@ void sim::save(char fname[128]) {
 		}
 
 		file.close();
-		cout << "Save Complete" << endl;
 	} else {
 		cout << "File Not Open" << endl;
 	}
 }
 
 void sim::pause() {
+	cout << "PAUSED" << endl;
 	char kin;
 	cin >> kin;
+}
+
+//print arrays
+void sim::print(double *arr, int size) {
+	cout << "[";
+	for(int a = 0; a < size; a++) {
+		cout << arr[a];
+
+		if(a < size-1) {
+			cout << ", ";
+		}
+	}
+	cout << "]" << endl;
 }
 
 #endif
